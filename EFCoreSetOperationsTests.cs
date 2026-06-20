e3dfe977ef43eb29d3c5f2794156c7158c9d48a6
grammophone <emmanuel.liossis@gmail.com>
2026-06-20 13:25:56 -0400

    Unit tests for batch operations.

---
+++ EFCoreSetOperationsTests.cs
@@ -0,0 +1,23 @@
+using Grammophone.DataAccess.Tests.Cases;
+using Grammophone.DataAccess.Tests.Domain;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Grammophone.DataAccess.Tests.Cases.EntityFrameworkCore
+{
+	/// <summary>
+	/// Entity Framework Core set-operation tests.
+	/// </summary>
+	[TestClass]
+	public class EFCoreSetOperationsTests : SetOperationsTestCases
+	{
+		#region Protected methods
+
+		/// <inheritdoc/>
+		protected override IMusicDomainContainer CreateDomainContainer()
+		{
+			return EFCoreTestFactory.CreateDomainContainer();
+		}
+
+		#endregion
+	}
+}
